18daff4b4ddfdcc654580ab5018672461f471788
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-03-10 16:17:12 -0300

    Initial commit

--- testeJava/src/testejava/TesteJava.java
+++ testeJava/src/testejava/TesteJava.java
@@ -3,6 +3,8 @@ package testejava;
 public class TesteJava {
     public static void main(String[] args) {
         System.out.print("Hello Git");
+        
+        System.out.print("Puxar");
     }
     
 }
